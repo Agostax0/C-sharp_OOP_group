e5d26cdfc5428e4ebbbf1e12e532319d6e0f5e7c
Alessandro Agosta - alessandro.agosta@studio.unibo.it <alessandro.agosta@studio.unibo.it>
2022-05-04 17:01:49 +0200

    added documentation

--- Agosta/IInputFormatterLogics.cs
+++ Agosta/IInputFormatterLogics.cs
@@ -3,10 +3,11 @@ namespace OOP21_Calculator.Agosta
 {
     internal interface IInputFormatterLogics
     {
+        ///<summary> This method reads its input value to the engine's memory.</summary>
         void Read(string input);
-
+        ///<summary> This method deletes the engine's memory last input value.</summary>
         void DeleteLast();
-
+        ///<summary> This method calculates the current state of its engine's memory</summary>
         void Calculate();
     }
 }
